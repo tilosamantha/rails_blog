5cc0023f630da86e011ee2cf8a7b2c85b40fdd06
Sam Tilo <tilosamantha@gmail.com>
2020-05-26 20:58:50 -0600

    end of lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :pages
+  root 'static_pages#home'
+  
+  # custom route - http verb url endpoint
+  get '/about', to: 'static_pages#about'
+  
+  resources :pages 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
